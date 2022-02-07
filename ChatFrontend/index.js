783b6d492d90a4f92453284d93074de5c258037d
SurajMondem <surajmondem@gmail.com>
2022-02-06 21:53:22 -0500

    Code Refactor to make Whatsapp

--- ChatFrontend/index.js
+++ ChatFrontend/index.js
@@ -1,28 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import socketClient  from "socket.io-client";
-import ChatBox from './Components/ChatBox';
-
-import './index.scss';
+import Login from './Components/Login';
+import Dashboard from './Components/Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import useLocalStorage from './hooks/useLocalStorage';
 
 const BACKEND_PORT = '8080';
 
 
 export const App = () => {
-  // const [response, setResponse] = useState("");
-
-  // useEffect(() => {
-    
-  //   const socket = socketClient(`http://localhost:${BACKEND_PORT}`);
-  //   socket.on("FromAPI", data => {
-  //     setResponse(data);
-  //   });
-
-  //   return () => socket.disconnect();
-  // }, []);
+  const [id, setId] = useLocalStorage('id');
 
   return (
-    <ChatBox />
+    <>
+      {id ? <Dashboard id={id} />: <Login onIdSubmit={setId}/>}
+      
+    </>
   )
 };
 
